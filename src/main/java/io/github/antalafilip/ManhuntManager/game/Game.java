44d6ae416fef120dba4bfe6d0fd6d6dcc5cb6a5c
Filip Antala <fifcostyle@gmail.com>
2021-03-06 19:41:46 +0100

    started work on Game class

--- src/main/java/io/github/antalafilip/ManhuntManager/game/Game.java
+++ src/main/java/io/github/antalafilip/ManhuntManager/game/Game.java
@@ -1,5 +1,17 @@
 package io.github.antalafilip.ManhuntManager.game;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.Date;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+public class Game {
+	private World world;
+
+	private ArrayList<Player> hunters;
+	private ArrayList<Player> runners;
+	
+	private Date startedAt;
+	
 }
